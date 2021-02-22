3e21f623a56285ae656f10af1c2a411ae0f04056
mhhasanmehedi <mmmehedi53@gmail.com>
2021-02-23 00:04:18 +0600

    component declare

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import reactDom from 'react-dom';
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done <code>src/App.js</code> and save to reload.
-        </p>
+        <p>I'm mehedi hasan</p>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
       </header>
     </div>
   );
+}
+function Person(){
+  const personStyle ={
+    border:"2px solid red",
+    margin:'30px',
+    padding:'20px'
+  }
+  return (
+    <div style={personStyle}>
+      <h1>Name: Sakib khan</h1>
+      <h3>Hero of the year</h3>
+    </div>
+  )
 }
 
 export default App;
